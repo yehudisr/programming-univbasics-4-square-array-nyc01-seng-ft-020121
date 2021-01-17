7a3b38e2da2f15400391daa9128670fd0524ed24
yehudis raitman <yehudisr@gmail.com>
2021-01-17 20:46:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@
 def square_array(array)
   counter = 0 
   new_array = []
-  while array.length < counter do
+  while counter < array.length do
   new_array.push(array[counter]**2)  
   counter += 1
   end  
